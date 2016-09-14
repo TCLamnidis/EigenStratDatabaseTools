e3c8bb899a2efa63e92ceadd59b169db8096ca6f
TCLamnidis <thisseass@gmail.com>
2016-09-14 18:44:24 +0200

    Added input checks. Added Extract function. Fixed bugs with grepping no samples or more than one sample.

--- EigenstratIndCheck.py
+++ EigenstratIndCheck.py
@@ -5,6 +5,7 @@ import sys, argparse, sh
 parser = argparse.ArgumentParser(usage="%(prog)s (-i <Input file prefix>) (-c <input ind file> | -R | -E) [-L <SAMPLE LIST> | -S Ind [-S Ind2]] <ouput target argument>" , description="Check two EigenStrat databases for duplicate individuals. Extract or remove individuals from an EigenStrat database.")
 parser._optionals.title = "Available options"
 parser.add_argument("-i", "--Input", type=str, metavar="<INPUT FILES PREFIX>", required=True, help="The desired input file prefix. Input files are assumed to be <INPUT PREFIX>.geno, <INPUT PREFIX>.snp and <INPUT PREFIX>.ind .")
+parser.add_argument("-o", "--Output", type=str, metavar="<OUTPUT FILES PREFIX>", required=True, help="The desired output file prefix. Three output files are created, <OUTPUT FILES PREFIX>.geno , <OUTPUT FILES PREFIX>.snp and <OUTPUT FILES PREFIX>.ind .")
 group = parser.add_mutually_exclusive_group(required=True)
 group2 = parser.add_mutually_exclusive_group(required=False)
 group.add_argument("-C", "--Check", type=argparse.FileType('r'), metavar="<INPUT FILE>", required=False, help="Check the -i .ind file and the second .ind file for duplicate individuals. Population assignment and/or individual sex are not checked, only individual names. Names are case sensitive.")
@@ -20,8 +21,12 @@ if args.Extract is True or args.Remove is True:
 
 IndFile = open(args.Input+".ind", "r")
 GenoFile = open(args.Input+".geno", "r")
-# SnpFile = open(args.Input+".snp", "r")
+SnpFile = open(args.Input+".snp", "r")
 o = sys.stdout
+IndOutFile = open(args.Output+".ind", "w")
+GenoOutFile = open(args.Output+".geno", "w")
+SnpOutFile = open(args.Output+".snp", "w")
+
 
 #Check function
 if args.Check != None:
@@ -43,6 +48,27 @@ if args.Check != None:
 	print ("#Duplicate individual check finished.", c, "duplicate individuals found.", sep=" ", file =o)
 	sys.exit(0)
 
+#Check for errors in input files
+##Check geno and snp compatibility
+lineNo = ""
+for line in sh.grep(sh.wc("-l", args.Input+".geno", args.Input+".snp"), args.Input):
+	if lineNo=="":
+		lineNo=line.strip().split()[0]
+	elif lineNo==line.strip().split()[0]:
+		break
+	elif lineNo!=line.strip().split()[0]:
+		raise IOError("Input .snp and .geno files do not match.")
+
+##Check geno and ind compatibility
+with open(args.Input+".geno", "r") as f:
+	for line in f:
+		print (len(line.strip()))
+		print(str(sh.wc("-l", args.Input+".ind")).strip().split()[0])
+		if str(len(line.strip())) == sh.wc("-l", args.Input+".ind").strip().split()[0]:
+			break
+		else:
+			raise IOError("Input .ind and .geno files do not match.")
+
 #Read sample names into list of individuals of interest, from either input option
 Samples = []
 if args.SampleList != None:
@@ -61,12 +87,21 @@ if args.Remove == True:
 	print ("Detected", len(Samples), "individuals for REMOVAL.", sep=" ", file=sys.stderr)
 elif args.Extract == True:
 	print ("Detected", len(Samples), "individuals for EXTRACTION.", sep=" ", file=sys.stderr)
+
 #Get sample index in geno and ind files.
 Index = {}
+Sex = {}
+Pop = {}
 for i in Samples:
-	for line in sh.grep(sh.cat("-n",args.Input+".ind"), "{}".format(i)):
+	for line in sh.grep(sh.cat("-n",args.Input+".ind"), "{}".format(i),_ok_code=[0,1]):
 		fields=line.strip().split()
-		Index[fields[1]]=(int(fields[0]) -1)
+		if fields[1] == i:
+			Index[fields[1]]=(int(fields[0]) -1)
+			Sex [fields[1]]=fields[2]
+			Pop [fields[1]]=fields[3]
+
+print (Pop)
+print (Sex)
 print (Index)
 print ("Indexed", len(Index), "individuals.", sep =" ", file=sys.stderr)
 
@@ -74,10 +109,42 @@ print ("Indexed", len(Index), "individuals.", sep =" ", file=sys.stderr)
 if args.Extract == True:
 	for line in GenoFile:
 		fields=line.strip()
-		for i in Index:
-			print (fields[Index[i]], end="")
-		print("")
+		for i in Samples:
+			if i in Index:
+				print (fields[Index[i]], end="", file=GenoOutFile)
+		print("",file=GenoOutFile)
+	
+	print ("\nIND FILE")
+	for i in Samples:
+		try: print (i, Sex[i], Pop[i], sep="\t", file=IndOutFile)
+		except KeyError:
+			print ("Individual \"",i,"\" not found in ",args.Input+".ind file.", sep="", file=sys.stderr)
+	
+	for line in SnpFile:
+		print (line, end="", file =SnpOutFile)
+	sys.exit(0)
 
-
+#Remove function
+#
+# if args.Remove == True:
+# 	for line in GenoFile:
+# 		fields=line.strip()
+# 		for i in range(len(fields)):
+# 			if i in Index.values():
+# 				continue
+# 			else:
+# 			print (fieldsi], end="", file=GenoOutFile)
+# 		print("",file=GenoOutFile)
+#
+# 	print ("\nIND FILE")
+# 	for i in Samples:
+# 		try: print (i, Sex[i], Pop[i], sep="\t", file=IndOutFile)
+# 		except KeyError:
+# 			print ("Individual \"",i,"\" not found in ",args.Input+".ind file.", sep="", file=sys.stderr)
+#
+# 	for line in SnpFile:
+# 		print (line, end="", file =SnpOutFile)
+# 	sys.exit(0)
+#
 
 
